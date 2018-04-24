fa0c3048b0ee8b0bf008a5ecbc805d991bfa54be
Santiago <santi@santi-HP>
2018-04-24 10:12:20 +0200

    Inicio

---
+++ src/Menu.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader; 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Menu {
+
+	public static void main(String[] args) throws IOException {
+		
+		
+		
+		
+		
+		
+		/*System.out.println("1. Iniciar sesión\n2. Entrar con mi usuario\n3. Salir");
+		String ruta = "/home/santi/Desktop/Julian.txt";
+        File archivo = new File(ruta);
+        BufferedWriter bw;
+        if(archivo.exists()) {
+            bw = new BufferedWriter(new FileWriter(archivo,true));
+            bw.write("El fichero de texto ya estaba creado.\n");
+        } else {
+            bw = new BufferedWriter(new FileWriter(archivo,true));
+            bw.write("Acabo de crear el fichero de texto.\n");
+        }
+        bw.close();
+        Scanner entrada = new Scanner(System.in);
+        entrada.nextInt();
+        System.out.println(entrada);
+        FileReader fr = null;
+        BufferedReader br = null;
+        fr = new FileReader (archivo);
+        br = new BufferedReader(fr);
+        String linea;
+        while((linea=br.readLine())!=null){
+           System.out.println(linea);
+        }
+        fr.close();*/
+    }
+}
